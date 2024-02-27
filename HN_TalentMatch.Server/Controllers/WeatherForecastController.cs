ead11616fc1e235d8784d8d55aaab92d26522ad8
Ермолаев Александр Алексеевич <ermolaev@rus-telecom.ru>
2024-02-27 18:06:17 +0300

    Бэкенд по маршруту "GetWeatherForecast" стал возвращать не рандомные данные, а фиксированные.

--- HN_TalentMatch.Server/Controllers/WeatherForecastController.cs
+++ HN_TalentMatch.Server/Controllers/WeatherForecastController.cs
@@ -21,6 +21,20 @@ namespace HN_TalentMatch.Server.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            List<WeatherForecast> result=new List<WeatherForecast>();
+            for (var index = 1; index < 6; index++)
+            {
+                result.Add(new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = 7*index,
+                    Summary = Summaries[index]
+                });
+            }
+
+            return result.ToArray();
+            
+            //OLD
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
